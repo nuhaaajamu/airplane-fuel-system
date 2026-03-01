e40ee9a305d16d820d08d799c7fd1da297882f2e
“Nuhaa <nuhaaajamu@gmail.com>
2026-03-01 17:05:41 -0500

    Implemented tester function for addTank()

--- mytest.cpp
+++ mytest.cpp
@@ -5,22 +5,23 @@ public:
     // Tests addTank function
     bool insertWhenEmpty(); // Evaluates if insertion is still successful even when list is empty.
     bool insertMultipleTanks();  // Evaluates whether the tank is properly added to the end of the list.
+    bool validateInputError(); // Handles the error cases of the input passed in (ID, capacity, fuel)
 };
 
 bool Tester::insertWhenEmpty(){
     // Ensure that a tank can be successfully added to an empty list.
-    FuelSys obj1;
-    bool result = obj1.addTank(5, 2500, 1000);
+    FuelSys obj;
+    bool result = obj.addTank(5, 2500, 1000);
     if (result == false) {
         cout << "Error: The tank was unable to be added" << endl;
         return false;
     }
 
     // Now, check if current and m_current were properly updated. They should be pointing to one another.
-    if (obj1.m_current == nullptr) {
+    if (obj.m_current == nullptr) {
         cout << "Error: m_current points to nullptr" << endl;
         return false;
-    }else if (obj1.m_current != obj1.m_current->m_next){
+    }else if (obj.m_current != obj.m_current->m_next){
         cout << "Error: m_current and m_current->m_next do not store the same address" << endl;
         return false;
     }
@@ -31,31 +32,31 @@ bool Tester::insertWhenEmpty(){
 
 bool Tester::insertMultipleTanks() {
     // Check that m_current is being updated properly as each tank is added by evaluating the ID.
-    FuelSys obj1;
+    FuelSys obj;
 
     // Add first tank and evaluate.
-    if (obj1.addTank(10, 3000, 500) == false) {
+    if (obj.addTank(10, 3000, 500) == false) {
         cout << "Error: First tank was not added successfully" << endl;
         return false;
-    }else if (obj1.m_current->m_tankID != 10) {
+    }else if (obj.m_current->m_tankID != 10) {
         cout << "Error: Incorrect data assigned to the first tank inserted" << endl;
         return false;
     }
 
     // Add second tank and evaluate.
-    if (obj1.addTank(20, 4000, 1000) == false) {
+    if (obj.addTank(20, 4000, 1000) == false) {
         cout << "Error: Second tank was not added successfully" << endl;
         return false;
-    }else if (obj1.m_current->m_tankID != 20) {
+    }else if (obj.m_current->m_tankID != 20) {
         cout << "Error: Incorrect data assigned to the second tank inserted" << endl;
         return false;
     }
 
     // Add third tank and evaulate.
-    if (obj1.addTank(30, 5000, 1500) == false) {
+    if (obj.addTank(30, 5000, 1500) == false) {
         cout << "Error: Third tank was not added successfully" << endl;
         return false;
-    }else if (obj1.m_current->m_tankID != 30) {
+    }else if (obj.m_current->m_tankID != 30) {
         cout << "Error: Incorrect data assigned to the third tank inserted" << endl;
         return false;
     }
@@ -64,17 +65,46 @@ bool Tester::insertMultipleTanks() {
     return true;
 }
 
+bool Tester::validateInputError() {
+    // 1. Pass in an invalid ID (less than zero) and evaluate if addTank() properly guards against it.
+    FuelSys obj1;
+    if (obj1.addTank(-1, 3000, 500) == true) {
+        cout << "Error: An invalid ID was assigned" << endl;
+        return false;
+    }
+
+    // 2. Pass in an invalid tank capacity (less than MIN_CAP) and evaluate if addTank() properly guards against it.
+    FuelSys obj2;
+    if (obj2.addTank(10, 1000, 500) == true) {
+        cout << "Error: An invalid tank capacity was assigned" << endl;
+        return false;
+    }
+
+    // 3. Pass in an invalid fuel capacity (fuel > capacity) and evaluate if addTank() properly guards against it.
+    FuelSys obj3;
+    if (obj3.addTank(10, 3000, 5000) == true) {
+        cout << "Error: An invalid amount of tank fuel was assigned" << endl;
+        return false;
+    }
+
+    cout << "Success: No valid input was assigned" << endl;
+    return true;
+}
+
 int main() {
     // Test addTank function
-
-    // 1. Ensure that a tank is added successfully to an empty list.
     cout << "===== Testing addTank() =====" << endl;
     Tester test;
 
+    // Ensure that a tank is added successfully to an empty list.
     cout << endl << "1. Inserting a tank in an empty list" << endl;
     test.insertWhenEmpty();
 
+    // Ensure that multiple can be added in the intended order.
     cout << endl << "2. Inserting multiple tanks consecutively" << endl;
     test.insertMultipleTanks();
 
+    // Ensure that input passed in is validated properly.
+    cout << endl << "3. Checking error cases for invalid input" << endl;
+    test.validateInputError();
 }
